046ecc7ba69ecf5ad6b495397e69bcac521cabf9
jmanueltorress <tjmanuel354@gmail.com>
2025-06-08 18:13:52 -0600

    add active links

--- src/js/functions.js
+++ src/js/functions.js
@@ -185,3 +185,31 @@ document.addEventListener('DOMContentLoaded', () => {
     lupa.style.display = 'none';
   });
 });
+
+//active-links
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll("section[id]");
+  const navLinks = document.querySelectorAll('.nav-main a, .mobile-nav-overlay a');
+
+  function activateLinkOnScroll() {
+    let scrollY = window.scrollY;
+
+    sections.forEach(current => {
+      const sectionHeight = current.offsetHeight;
+      const sectionTop = current.offsetTop - 100;
+      const sectionId = current.getAttribute("id");
+
+      if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+        navLinks.forEach(link => {
+          link.classList.remove("active");
+          if (link.getAttribute("href") === `#${sectionId}`) {
+            link.classList.add("active");
+          }
+        });
+      }
+    });
+  }
+
+  window.addEventListener("scroll", activateLinkOnScroll);
+});
+
